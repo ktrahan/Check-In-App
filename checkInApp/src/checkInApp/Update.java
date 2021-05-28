86e53a44e21bea76710e78388f781f867940a6e3
Hunter-suhonen <84864071+Hunter-suhonen@users.noreply.github.com>
2021-05-28 09:20:57 -0700

    Update Update.java

--- checkInApp/src/checkInApp/Update.java
+++ checkInApp/src/checkInApp/Update.java
@@ -1,1 +1,18 @@
-
+public class Update
+{
+  private String studentName;
+  private boolean status;
+  public Update(String studentName, boolean status)
+  {
+    this.studentName = studentName;
+    this.status = status;
+  }
+  public String getStudentName()
+  {
+    return studentName;
+  }
+  public boolean getStatus()
+  {
+    return status;
+  }
+}
