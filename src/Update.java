789f8948822a0909d04c9b25f8f9c0b7f7aea006
AdinAck <adinackerman@gmail.com>
2021-05-28 10:39:08 -0700

    restored Update.java

---
+++ src/Update.java
@@ -0,0 +1,17 @@
+public class Update {
+    private String studentName;
+    private boolean status;
+
+    public Update(String studentName, boolean status) {
+        this.studentName = studentName;
+        this.status = status;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+}
