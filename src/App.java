9ae47c87ecb3314945545c05bd133718c2d2e92c
NiallRindt <60717703+NiallRindt@users.noreply.github.com>
2021-06-08 10:33:20 -0700

    Create App.java
    Created the main loop and inlcuded support for the slave setup. Also put in placeholder code to get whether or not the device is a slave.

--- src/App.java
+++ src/App.java
@@ -2,5 +2,25 @@
 Main program
 */
 class App {
-
+  public static void main(String[] args){
+    
+    //For the GUI group to get user input (slave or master) the following scanner is temporary
+	  Scanner device = new Scanner(System.in);
+	  System.out.println(“IS THIS DEVICE A MASTER OR SLAVE”);
+	  String deviceResponse = device.nextLine();
+    
+    //check if device is a slave then create and setup the slave
+    if(deviceResponse.equals(slave)){
+  		Slave slaveDevice = new Slave(/*whatever the slave IP adress is*/, /* whater the master IP adress is*/);
+      slaveDevice.setupSlave(); 
+    }
+    
+    while(true){
+    
+    //main loop, put that is run continuously here
+      
+    }
+    
+    
+  }
 }
